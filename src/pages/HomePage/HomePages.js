5af794591647949a936c9afcc5001e0030cc38f5
Zak-11 <dianazakov@rambler.ru>
2021-12-14 01:39:33 +0300

    "header"

--- src/pages/HomePage/HomePages.js
+++ src/pages/HomePage/HomePages.js
@@ -14,6 +14,7 @@ import Wer from './../../image/image1 (4).jpeg'
 import Ferta from './../../image/image0 (13).jpeg'
 import Fert from './../../image/image0 (3).jpeg'
 import Dert from './../../image/AnyConv.com__1 (7).jpeg'
+import Trr from './../../image/img.png'
 import PowersLap from '../../Videos/pexels-kampus-production-8037388.mp4'
 
 
@@ -24,29 +25,21 @@ const HomePages = () => {
             <div className="intro">
                 <div className="video">
                     <video className="video__media" loop muted autoPlay>
-                        <source src={PowersLap}    type="video/mp4"/>
+                        <source src={PowersLap} type="video/mp4"/>
                     </video>
                 </div>
 
                 <div className="intro_content">
-                    <div className="container">
-                        <h1>Kristina Turdo</h1>
-                        <p>Photograph Artist Creative person in everything.</p>
+                    <h1>Kristina Turdo</h1>
 
-
-                    </div>
                 </div>
             </div>
+
             <div className={'home_page'}>
                 <div className={'description'}>
 
-                    <div className="intro">
-                        <div className="video">
-                            <video className="video__media" loop muted autoPlay>
-                                <source src={PowersLap}    type="video/mp4"/>
-                            </video>
-                        </div>
-                    </div>
+                    <p>An artist, photographer, <br/>designer
+                        is a creative <br/>person in everything.</p>
                 </div>
 
             </div>
